fdf932133ce10f1a3902b7aec67a53cdfb198d1f
Kim Konstantin <k.konstantin.27.21@gmail.com>
2023-11-20 16:24:04 +0900

    create AppUserDetails

---
+++ src/main/java/cloudFileStorage/security/AppUserDetails.java
@@ -0,0 +1,52 @@
+package cloudFileStorage.security;
+
+import cloudFileStorage.models.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AppUserDetails implements UserDetails {
+    private final AppUser appUser;
+
+    @Autowired
+    public AppUserDetails(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.appUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.appUser.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
