33c15466e5b5780e4fffe1695591e350a1d2ef52
Kim Konstantin <k.konstantin.27.21@gmail.com>
2023-11-30 12:38:41 +0900

    create BucketService

--- src/main/java/cloudFileStorage/configs/MinioClientConfig.java
+++ src/main/java/cloudFileStorage/configs/MinioClientConfig.java
@@ -1,10 +1,13 @@
 package cloudFileStorage.configs;
 
 import io.minio.MinioClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioClientConfig {
+
+    @Bean
     public MinioClient buildMinioClient() {
         return MinioClient.builder()
                         .endpoint("http://127.0.0.1:9000")
--- src/main/java/cloudFileStorage/services/StorageService.java
+++
@@ -1,9 +0,0 @@
-package cloudFileStorage.services;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-@Component
-public class StorageService {
-
-}
